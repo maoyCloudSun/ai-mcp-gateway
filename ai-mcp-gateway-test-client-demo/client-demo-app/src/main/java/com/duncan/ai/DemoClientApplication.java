a6d0b0220d1303735400dacf82ef0ba07bd5b032
SunDuncan <maoycloudsun@gmail.com>
2026-09-13 23:34:46 +0800

    feat(新增Server以及ClientDemo):

---
+++ ai-mcp-gateway-test-client-demo/client-demo-app/src/main/java/com/duncan/ai/DemoClientApplication.java
@@ -0,0 +1,11 @@
+package com.duncan.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoClientApplication.class, args);
+    }
+}
